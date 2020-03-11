5d661d2a57018d803065988d7c2fbe233e64be16
Dylan Hoogduin <dylanhoogduin@Dylans-MBP.fritz.box>
2020-03-11 15:19:26 +0100

    make db seeder

---
+++ Seeder.py
@@ -0,0 +1,80 @@
+import os
+from Database import db
+from dotenv import load_dotenv
+import settings
+
+class Seeder(db):
+
+    def __init__(self,host, username, password, database, gene_ref_file, gene_count_file):
+        super().__init__(host, username, password)
+        self.database = database
+        self.gene_ref_file= gene_ref_file
+        self.gene_count_file= gene_count_file
+
+    def insert_gene_ref(self):
+        self.connect_to_db(self.database)
+        with open(self.gene_ref_file) as f:
+            for line in f:
+                line = line.replace('\n', '').replace('\r', '')
+                items = line.split(';')
+                try:
+                    self.connection.execute("INSERT INTO gene (ensg, symbol, description) VALUES ('{ensg}', '{symbol}', '{desc}')".format(
+                        ensg=items[0], symbol=items[2], desc=items[1]
+                    ))
+                except:
+                    print('Entry already exist or error in adding record: {gene}'.format(gene=items[0]))
+        self.connection.close()
+        print('All genes are inserted')
+
+    def prepare_data(self, stage_file, tissue_file):
+        stages = {}
+        tissues = {}
+        with open(stage_file) as f:
+            for line in f:
+                line = line.replace('\n', '').replace('\r', '')
+                line = line.split(',')
+                stages[line[4]] = line[0]
+        f.close()
+        with open(tissue_file) as f:
+            for line in f:
+                line = line.replace('\n', '').replace('\r', '')
+                line = line.split(',')
+                tissues[line[4]] = line[0]
+        f.close()
+        return stages, tissues
+
+    def insert_count(self, stage_file, tissue_file):
+        stages, tissues = self.prepare_data(stage_file,tissue_file )
+        self.connect_to_db(self.database)
+        self.connection.execute('DELETE FROM transcript;')
+        with open(self.gene_count_file) as f:
+            for line in f:
+                line = line.replace('\n', '').replace('\r', '')
+                items = line.split(';')
+                try:
+                    result = self.connection.execute("SELECT id FROM gene WHERE ensg = '{gname}'".format(gname=items[0]))
+                    self.connection.execute("INSERT INTO transcript (gene, stage, tissue, count) VALUES ('{gene}', '{stage}', '{tissue}', '{count}')".format(
+                        gene=[item[0] for item in result][0], stage=stages[items[2]], tissue=tissues[items[1]],
+                        count=items[3]
+                    ))
+                except:
+                    print('!Error while adding adding count record: {gene}'.format(gene=items[0]))
+        self.connection.close()
+
+if __name__ == '__main__':
+    HOST = os.getenv("HOST")
+    USERNAME = os.getenv("USERNAME")
+    PASSWORD = os.getenv("PASSWORD")
+    DATABASE = os.getenv("DATABASE")
+    REF_FILE = os.getenv("REF_FILE")
+    COUNT_FILE = os.getenv("COUNT_FILE")
+    seeder = Seeder(
+        host=HOST,
+        username=USERNAME,
+        password=PASSWORD,
+        database=DATABASE,
+        gene_ref_file=REF_FILE,
+        gene_count_file=COUNT_FILE,
+    )
+    seeder.insert_gene_ref()
+    seeder.insert_count(stage_file='datasets/stage.csv', tissue_file='datasets/tissue.csv')
